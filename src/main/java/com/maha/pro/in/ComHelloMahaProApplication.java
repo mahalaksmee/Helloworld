8d37b72f76efe698362016c1ca00f2336dac21be
mahalakshmee <mahalakshmee6@gmail.com>
2022-07-21 22:16:52 +0530

    initial commit

---
+++ src/main/java/com/maha/pro/in/ComHelloMahaProApplication.java
@@ -0,0 +1,14 @@
+package com.maha.pro.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComHelloMahaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComHelloMahaProApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
